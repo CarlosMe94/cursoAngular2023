f239fc582800bfa58a878cf0085bf8c1187b3613
Carlos Meijomil <carlos.meijo@gmail.com>
2023-05-02 19:14:25 +0200

    hasta clase 45

--- bases/src/app/heroes/hero/hero.component.ts
+++ bases/src/app/heroes/hero/hero.component.ts
@@ -5,4 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css'],
 })
-export class HeroComponent {}
+export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 123;
+  }
+
+  resetForm(): void {
+    // this.name = 'ironman';
+    this.age = 45;
+
+    document.querySelectorAll('h1')!.forEach((element) => {
+      element.innerHTML = '<h1>Desde angular</h1>';
+    });
+  }
+}

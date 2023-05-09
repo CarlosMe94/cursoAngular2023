31b8d860284579045ada576c79654d66447b9637
Carlos Meijomil <carlos.meijo@gmail.com>
2023-05-09 13:48:11 +0200

    hasta clase 115

---
+++ countryApp/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
+})
+export class AppModule {}

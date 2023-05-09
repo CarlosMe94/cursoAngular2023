e67fd9507002f44ee8543fbbc20b3692103c4ef2
Carlos Meijomil <carlos.meijo@gmail.com>
2023-05-09 18:50:34 +0200

    hasta clase 119

--- countryApp/src/app/app.module.ts
+++ countryApp/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, HttpClientModule],
 })
 export class AppModule {}

39625c2a558488f686ff7efad17c7163ab2c2426
Carlos Meijomil <carlos.meijo@gmail.com>
2023-05-22 18:37:24 +0200

    fin seccion 18 - validaciones formulario

--- formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
+import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            this.validatorsService.firstNameAndLastnamePattern
+          ),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+        [this.emailValidator],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorsService.cantBeStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2'),
+      ],
+    }
+  );
 
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidatorService
+  ) {}
+
+  isValidField(field: string) {
+    // TODO: Obtener validacion desde servicio
+
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit(): void {
+    this.myForm.markAllAsTouched();
+  }
 }

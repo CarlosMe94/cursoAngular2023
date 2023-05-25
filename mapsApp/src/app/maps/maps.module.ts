29e91843d4d12cbfd7666d706d23a4e72d9ea80e
Carlos Meijomil <carlos.meijo@gmail.com>
2023-05-25 13:53:12 +0200

    fin seccion 20, hasta clase 287 maps

---
+++ mapsApp/src/app/maps/maps.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MapsRoutingModule } from './maps-routing.module';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+(mapboxgl as any).accessToken =
+  'pk.eyJ1IjoiY2FybG9zbTk0IiwiYSI6ImNsaTMxam94cDJkZWEzbG13ZDhteXIycnQifQ.MWubw_Nj9qK697vZcQBEIQ';
+
+@NgModule({
+  declarations: [
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent,
+    FullScreenPageComponent,
+    MarkersPageComponent,
+    PropertiesPageComponent,
+    ZoomRangePageComponent,
+  ],
+  imports: [CommonModule, MapsRoutingModule],
+})
+export class MapsModule {}
---
+++ mapsApp/src/environments/environment.ts
@@ -0,0 +1,4 @@
+
+export const environment = {
+  mapbox_key: "pk.eyJ1IjoiY2FybG9zbTk0IiwiYSI6ImNsaTMxam94cDJkZWEzbG13ZDhteXIycnQifQ.MWubw_Nj9qK697vZcQBEIQ"
+}

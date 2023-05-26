a3c0ffb086582edd89ca5f2d66d158d2758c3f10
Carlos Meijomil <carlos.meijo@gmail.com>
2023-05-26 14:00:08 +0200

    hasta clase 294

--- mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
 @Component({
   templateUrl: './markers-page.component.html',
-  styleUrls: ['./markers-page.component.css']
+  styleUrls: ['./markers-page.component.css'],
 })
 export class MarkersPageComponent {
+  @ViewChild('map') divMap?: ElementRef;
 
+  public markers: MarkerAndColor[] = [];
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(
+    -8.106724996434366,
+    42.65680387896637
+  );
+
+  ngAfterViewInit(): void {
+    console.log(this.divMap);
+    if (!this.divMap) {
+      throw 'Elemento no encontrado';
+    }
+
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat,
+      zoom: this.zoom, // starting zoom
+    });
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Fernadno Herrera';
+    // const marker = new Marker({ element: markerHtml })
+    //   .setLngLat(this.currentLngLat)
+    //   .addTo(this.map);
+  }
+
+  createMarker() {
+    if (!this.map) {
+      return;
+    }
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const lngLat = this.map.getCenter();
+    this.addMarker(lngLat, color);
+  }
+  addMarker(logLata: LngLat, color: string) {
+    if (!this.map) {
+      return;
+    }
+
+    const marker = new Marker({
+      color,
+      draggable: true,
+    })
+      .setLngLat(logLata)
+      .addTo(this.map);
+    this.markers.push({ color, marker });
+  }
+
+  deleteMarker(index: number) {
+    this.markers[index].marker.remove();
+    this.markers.splice(index, 1);
+  }
+
+  flyTo(marker: Marker) {
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat(),
+    });
+  }
 }
--- mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,81 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
-  styleUrls: ['./zoom-range-page.component.css']
+  styleUrls: ['./zoom-range-page.component.css'],
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('map') divMap?: ElementRef;
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(
+    -8.106724996434366,
+    42.65680387896637
+  );
 
+  ngAfterViewInit(): void {
+    console.log(this.divMap);
+    if (!this.divMap) {
+      throw 'Elemento no encontrado';
+    }
+
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat,
+      zoom: this.zoom, // starting zoom
+    });
+    this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.map?.remove();
+  }
+
+  mapListeners() {
+    if (!this.map) {
+      throw 'Mapa no iniciado';
+    }
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) {
+        return;
+      }
+      this.map!.zoomTo(18);
+    });
+
+    this.map.on('move', (ev) => {
+      this.currentLngLat = this.map!.getCenter();
+      const { lng, lat } = this.currentLngLat;
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChange(value: string) {
+    this.zoom = Number(value);
+
+    this.map?.zoomTo(this.zoom);
+  }
 }

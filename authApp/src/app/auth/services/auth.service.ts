060264ace24baf75890723330f54546bccaf766c
Carlos Meijomil <carlos.meijo@gmail.com>
2023-06-09 13:43:57 +0200

    fin seccion 26

--- authApp/src/app/auth/services/auth.service.ts
+++ authApp/src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environments } from 'src/app/environments/enviroments';
 import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { CheckTokenResponse } from '../interfaces/check-token.response';
 
 @Injectable({
   providedIn: 'root',
@@ -20,7 +21,16 @@ export class AuthService {
   public currentUser = computed(() => this._current());
   public authStatus = computed(() => this._authStatus());
 
-  constructor() {}
+  private setAuthentication(user: User, token: string): boolean {
+    this._current.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+    return true;
+  }
+
+  constructor() {
+    this.checkAuthStatus().subscribe();
+  }
 
   login(email: string, password: string): Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
@@ -28,17 +38,40 @@ export class AuthService {
     const body = { email, password };
 
     return this.http.post<LoginResponse>(url, body).pipe(
-      tap(({ user, token }) => {
-        this._current.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-        localStorage.setItem('token', token);
-        console.log({ user, token });
-      }),
-      map(() => true),
+      map(({ user, token }) => this.setAuthentication(user, token)),
 
       catchError((err) => {
         return throwError(() => err.error.message);
       })
     );
+  }
+
+  checkAuthStatus(): Observable<boolean> {
+    const url = `${this.baseUrl}/auth/check-token`;
+
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      this.logout();
+      return of(false);
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
+      map(({ token, user }) => {
+        return this.setAuthentication(user, token);
+      }),
+      catchError(() => {
+        this._authStatus.set(AuthStatus.notAuthenticated);
+        return of(false);
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this._current.set(null);
+    this._authStatus.set(AuthStatus.notAuthenticated);
   }
 }
